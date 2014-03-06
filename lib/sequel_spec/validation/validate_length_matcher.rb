640eff44fa68a961a7e8a85f816f4d04f22e9d07
Adrià Planas <adriaplanas@liquidcodeworks.com>
2014-03-06 10:09:26 +0100

    Unify all length matchers into a single matcher

--- lib/sequel_spec/validation/validate_length_matcher.rb
+++ lib/sequel_spec/validation/validate_length_matcher.rb
@@ -1,40 +1,77 @@
 module SequelSpec
   module Matchers
     module Validation
-      class ValidateLengthMatcher
-        def initialize(attribute)
-          unless attribute
-            raise ArgumentError, "You should specify an attribute"
-          end
+      class ValidateLengthMatcher < ValidateMatcher
+        attr_reader :validation_type
 
-          @attribute = attribute
+        def description
+          desc = "validate length of #{@attribute.inspect} "
+          desc << case validation_type
+          when :validates_exact_length then "is exactly"
+          when :validates_min_length   then "is greater than or equal to"
+          when :validates_max_length   then "is less than or equal to"
+          when :validates_length_range then "is included in"
+          end << " #{@additionnal.inspect}"
+
+          desc << " with option(s) #{hash_to_nice_string @options}" unless @options.empty?
+          desc
         end
 
-        def matches?
-          raise ArgumentError, "You should specify the type of validation using #is, #is_at_least, #is_at_most or #is_between"
+        def additionnal_param_required?
+          true
+        end
+
+        def additionnal_param_check
+          unless validation_type && @additionnal
+            raise ArgumentError, "You should specify the type of validation using #is, #is_at_least, #is_at_most or #is_between"
+          end
         end
 
         def is(value)
-          ValidateExactLengthMatcher.new(value, @attribute)
+          unless value.is_a?(Fixnum)
+            raise ArgumentError, "#is expects a Fixnum, #{value.class} given"
+          end
+
+          @additionnal = value
+          @validation_type = :validates_exact_length
+          self
         end
 
         alias :is_equal_to :is
 
         def is_at_least(value)
-          ValidateMinLengthMatcher.new(value, @attribute)
+          unless value.is_a?(Fixnum)
+            raise ArgumentError, "#is_at_least expects a Fixnum, #{value.class} given"
+          end
+
+          @additionnal = value
+          @validation_type = :validates_min_length
+          self
         end
 
         def is_at_most(value)
-          ValidateMaxLengthMatcher.new(value, @attribute)
+          unless value.is_a?(Fixnum)
+            raise ArgumentError, "#is_at_most expects a Fixnum, #{value.class} given"
+          end
+
+          @additionnal = value
+          @validation_type = :validates_max_length
+          self
         end
 
         def is_between(value)
-          ValidateLengthRangeMatcher.new(value, @attribute)
+          unless value.is_a?(Range)
+            raise ArgumentError, "#is_between expects a Range, #{value.class} given"
+          end
+
+          @additionnal = value
+          @validation_type = :validates_length_range
+          self
         end
       end
 
-      def validate_length(attribute)
-        ValidateLengthMatcher.new(attribute)
+      def validate_length(*args)
+        ValidateLengthMatcher.new(*args)
       end
 
       alias :validate_length_of :validate_length
